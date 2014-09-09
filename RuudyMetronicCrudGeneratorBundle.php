9d109d33f233cb7bb0b0c6093fc77212ce85790a
Ruudy <daniel.abad85@gmail.com>
2014-09-09 16:44:32 +0200

    Changed Bundle class name

--- RuudyMetronicCrudGeneratorBundle.php
+++ RuudyMetronicCrudGeneratorBundle.php
@@ -4,6 +4,6 @@ namespace Ruudy\MetronicCrudGeneratorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MetronicCrudGeneratorBundle extends Bundle
+class RuudyMetronicCrudGeneratorBundle extends Bundle
 {
 }
